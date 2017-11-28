e12450542104ab7ccecdeeaa9db31861d99b0196
= <=>
2017-11-28 11:30:39 +0530

    First commit

---
+++ app/src/main/java/com/waytojava/floatingactionbutton/MainActivity.java
@@ -0,0 +1,25 @@
+package com.waytojava.floatingactionbutton;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "FAB is clicked !", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
